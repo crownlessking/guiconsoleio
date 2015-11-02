99965bc8d9a28098e68cca7708b72afe4d661872
crownlessking <fitz_kern@hotmail.com>
2015-11-02 13:05:52 -0400

    C class simulates console I/O in GUI

---
+++ Main.java
@@ -0,0 +1,20 @@
+/*
+ * @file
+ */
+package consoleio;
+
+/**
+ *
+ * @author riviere
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        C.io.setTitle("GUI Console Test!");
+    }
+
+}
