e18f972025cfaf8866ef6b5d188466f9c8fbc35a
crownlessking <fitz_kern@hotmail.com>
2015-11-02 13:42:30 -0400

    C class simulates console I/O in GUI

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@
  */
 package consoleio;
 
+import java.awt.Color;
+
 /**
  *
  * @author riviere
@@ -14,7 +16,21 @@ public class Main {
      */
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        C.io.setTitle("GUI Console Test!");
+        C.io.setTitle("Example"); // Sets window title
+        
+        C.io.println("Hello World!");
+        
+        C.io.print("Enter your dog's name:");
+        String name = C.io.nextLine(); // Capture user input from keyboard
+        
+        C.io.print("Enter your dog's age:");
+        int age = C.io.nextInt(); // Capture user input from keyboard
+        
+        C.io.print("Your dog name is ");
+        C.io.print(name, Color.YELLOW);
+        C.io.print(" and it's ");
+        C.io.print(age, Color.CYAN);
+        C.io.println(" year(s) old.");
     }
 
 }
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-C.java Class
+line # C.java Class
 
-Description:
+line Description:
+line C.java will move your console base application into a GUI window.
 
+line How to use:
+line Simply add the C.java class to your project and use at least one of its methods.
+line e.g.
 
-version 
+```java
+C.io.setTitle() // sets the window title
+```
+
+line version 1.0
